f27b6866a71764b60fb35bed35ac8418e88de2c8
donghyun-git <donghyun1113@hanmail.net>
2023-06-22 02:29:11 +0900

    feat: 로그인 api 요청 함수 추가

--- src/apis/authService.js
+++ src/apis/authService.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-export const regiser = async (user) => {
+export const register = async (user) => {
   const { data } = await axios.post("/auth/register", user);
+  return data;
+};
+
+export const logIn = async (user) => {
+  const { data } = await axios.post("/auth/login", user);
   return data;
 };
