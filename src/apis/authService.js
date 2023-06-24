b5c323fb69b0730fb1fcd43f5d8f1d1b2d8e3d7b
donghyun-git <donghyun1113@hanmail.net>
2023-06-24 21:14:29 +0900

    fix: api 요청 uri 환경변수 적용

--- src/apis/authService.js
+++ src/apis/authService.js
@@ -1,11 +1,15 @@
 import axios from "axios";
+import * as env from "../../env.config";
 
 export const register = async (user) => {
-  const { data } = await axios.post("/auth/register", user);
+  const { data } = await axios.post(
+    `${env.VITE_SERVER_URL}/auth/register`,
+    user
+  );
   return data;
 };
 
 export const logIn = async (user) => {
-  const { data } = await axios.post("/auth/login", user);
+  const { data } = await axios.post(`${env.VITE_SERVER_URL}/auth/login`, user);
   return data;
 };
