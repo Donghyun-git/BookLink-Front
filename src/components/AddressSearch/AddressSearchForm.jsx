5e478ee3ab9cc61ce263873c84774d04bb0d1bc4
jyj1111 <jyj202040@gmail.com>
2023-06-26 00:34:37 +0900

    feat: 주소찾기 컴포넌트 생성

---
+++ src/components/AddressSearch/AddressSearchForm.jsx
@@ -0,0 +1,33 @@
+import DaumPostcode from "react-daum-postcode";
+const AddressSearchForm = ({ setBasicAddress }) => {
+  const handleComplete = (data) => {
+    let fullAddress = data.address;
+    let extraAddress = "";
+    // console.log(data);
+    if (data.addressType === "R") {
+      if (data.bname !== "") {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== "") {
+        extraAddress +=
+          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+    }
+    setBasicAddress(fullAddress);
+    console.log(fullAddress); // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
+  };
+
+  const handleSearch = (data) => {
+    console.log(data);
+  };
+  return (
+    <DaumPostcode
+      onComplete={handleComplete}
+      onSearch={handleSearch}
+      //{...props}
+    />
+  );
+};
+
+export default AddressSearchForm;
