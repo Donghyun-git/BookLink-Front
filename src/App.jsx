d35531ed62c196ca52ef0ffa805234ac7f53c28f
donghyun-git <donghyun1113@hanmail.net>
2023-06-28 17:16:01 +0900

    style: 프리티어 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import "./App.css";
-import "../public/css/default.css";
-import Router from "./routes/Router";
+import './App.css';
+import '../public/css/default.css';
+import Router from './routes/Router';
 function App() {
   return (
     <>
