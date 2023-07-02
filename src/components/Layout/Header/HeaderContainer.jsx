1d17dbd75a47532cbd7f637582d77f83ccb3679d
donghyun-git <donghyun1113@hanmail.net>
2023-07-02 18:27:49 +0900

    chore: header 검색창 디자인

--- src/components/Layout/Header/HeaderContainer.jsx
+++ src/components/Layout/Header/HeaderContainer.jsx
@@ -1,22 +1,50 @@
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Logo from '../../../images/BookLink_Logo.svg';
 import * as Styled from './HeaderStyled';
-import { Link } from 'react-router-dom';
 
 const HeaderContainer = () => {
+  const path = useLocation().pathname;
+  const [pathName, setPathName] = useState(path);
+
+  useEffect(() => {
+    setPathName(path);
+  }, [path]);
+
   return (
     <Styled.HeaderContainer>
       <Styled.LogoDiv>
-        <Link to="/books">
+        <Styled.StyledLink to="/books">
           <Styled.Img src={Logo} />
-        </Link>
+        </Styled.StyledLink>
       </Styled.LogoDiv>
       <div>
-        <Styled.StyledLink to="/books">책방</Styled.StyledLink>
-        <Styled.LinkWithMargin to="/book-meetings">
+        <Styled.StyledLink
+          to="/books"
+          active={(pathName === '/books').toString()}
+        >
+          책방
+        </Styled.StyledLink>
+        <Styled.LinkWithMargin
+          to="/book-meetings"
+          active={(pathName === '/book-meetings').toString()}
+        >
           독서모임
         </Styled.LinkWithMargin>
-        <Styled.StyledLink to="/mypage">마이페이지</Styled.StyledLink>
+        <Styled.StyledLink
+          to="/mypage"
+          active={(pathName === '/mypage').toString()}
+        >
+          마이페이지
+        </Styled.StyledLink>
       </div>
+      <Styled.SearchDiv>
+        <Styled.CategorySelect>
+          <option value="책">책</option>
+          <option value="게시글">게시글</option>
+        </Styled.CategorySelect>
+        <Styled.SearchInput type="search" placeholder="책을 검색해보세요!" />
+      </Styled.SearchDiv>
     </Styled.HeaderContainer>
   );
 };
--- src/components/Layout/Header/HeaderStyled.js
+++ src/components/Layout/Header/HeaderStyled.js
@@ -10,18 +10,19 @@ export const Header = styled.header`
 
 // [ HeaderContainer ]
 export const HeaderContainer = styled.div`
-  width: 89.286rem;
+  width: 102.857rem;
   margin: 0 auto;
-  padding: 1rem 2.286rem;
+  /* padding: 1rem 2.286rem; */
   text-align: center;
   display: flex;
-  justify-content: space-between;
   align-items: center;
 `;
 
 export const LogoDiv = styled.div`
-  width: 12.214rem;
-  height: 1.929rem;
+  width: 16.28571rem;
+  height: 2.5rem;
+  padding: 2.36rem 0 2.29rem 0;
+  margin-right: 4.29rem;
 `;
 
 export const Img = styled.img`
@@ -30,11 +31,70 @@ export const Img = styled.img`
 `;
 
 export const StyledLink = styled(Link)`
-  font-size: 1.286rem;
-  font-weight: 400;
-  color: #848484;
+  color: #d9d9d9;
+  font-size: 1.14286rem;
+  font-weight: 700;
+  color: ${(props) => (props.active === 'true' ? '#00E37D' : '#d9d9d9')};
 `;
 
 export const LinkWithMargin = styled(StyledLink)`
-  margin: 0 3.929rem;
+  margin: 0 2.57rem;
+`;
+
+export const SearchDiv = styled.div`
+  position: relative;
+  margin-left: 11.43rem;
+  &:before {
+    content: '';
+    width: 1.2px;
+    height: 1.71429rem;
+    background-color: rgba(217, 217, 217, 1);
+    position: absolute;
+    top: 0.38rem;
+    left: 6.43rem;
+  }
+`;
+
+export const SearchInput = styled.input`
+  && {
+    padding-left: 0;
+    padding-right: 0;
+    width: 32.64286rem;
+    height: 2.57143rem;
+    border: 2px solid transparent;
+    border-radius: 0.571rem;
+    text-indent: 1.071rem;
+    font-size: 0.714rem;
+    background-image: linear-gradient(#fff, #fff),
+      linear-gradient(to right, rgba(0, 227, 125, 1), rgba(0, 60, 116, 1));
+    background-origin: border-box;
+    background-clip: content-box, border-box;
+    text-indent: 7.3rem;
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const CategorySelect = styled.select`
+  position: absolute;
+  bottom: 0;
+  left: -0.7rem;
+  && {
+    width: 6.86rem;
+    height: 2.57143rem;
+    padding: 0;
+    margin: 0;
+    border: 0;
+    color: #000;
+    text-align: center;
+    font-size: 0.714rem;
+    font-weight: 500;
+    background-repeat: no-repeat;
+    background-position: right 0.48rem center;
+    background-size: 0.85714rem auto;
+    &:focus {
+      outline: none;
+    }
+  }
 `;
