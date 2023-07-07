b5b2dcc36498b32524061dd743aadd3d9a9fdcdc
donghyun-git <donghyun1113@hanmail.net>
2023-07-07 21:37:18 +0900

    feat: redux 초기 세팅 추가

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+export { store };
