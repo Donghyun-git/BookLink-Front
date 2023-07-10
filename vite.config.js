254f99e2d88df12dee734f1a0c89b1c02008d4d4
jyj1111 <jyj202040@gmail.com>
2023-07-10 13:07:53 +0900

    chore: proxy 설정

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
 // https://vitejs.dev/config/
 
@@ -8,4 +8,11 @@ dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/v1/': {
+        target: 'http://43.202.83.3:8080/',
+      },
+    },
+  },
 });
