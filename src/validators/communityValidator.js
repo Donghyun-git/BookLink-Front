1217da541bb14d6a24b9ac9ac96dbd7616de0b5d
jyj1111 <jyj202040@gmail.com>
2023-07-11 18:06:33 +0900

    feat: 커뮤니티관련 유효성 추가

---
+++ src/validators/communityValidator.js
@@ -0,0 +1,7 @@
+import * as yup from 'yup';
+
+export const bookClubRegisterSchema = yup.object().shape({
+  title: yup.string().required('제목을 작성해주세요'),
+  location: yup.string().required('위치를 작성해주세요'),
+  content: yup.string().required('내용을 작성해주세요'),
+});
