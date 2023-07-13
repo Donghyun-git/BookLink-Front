f59cbe350d8ed543b29c2981913f65f2833b6731
donghyun-git <donghyun1113@hanmail.net>
2023-07-14 00:07:49 +0900

    chore: 환경변수 추가

--- env.config.js
+++ env.config.js
@@ -1,1 +1,8 @@
-export const { VITE_SERVER_URL } = import.meta.env;
+export const {
+  VITE_SERVER_URL,
+  VITE_APP_CLIENT_SECRET,
+  VITE_APP_REST_API_KEY,
+  VITE_APP_REDIRECT_URL,
+} = import.meta.env;
+
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${VITE_APP_REST_API_KEY}&redirect_uri=${VITE_APP_REDIRECT_URL}&response_type=code`;
