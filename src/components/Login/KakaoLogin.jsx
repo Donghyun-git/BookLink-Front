65eb27d0dda603fedddd7e0ddceac052947da810
donghyun-git <donghyun1113@hanmail.net>
2023-07-14 00:11:04 +0900

    style: 주석제거

--- src/components/Login/KakaoLogin.jsx
+++ src/components/Login/KakaoLogin.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useSearchParams, useNavigate } from 'react-router-dom';
-// import { axiosAuthInstance } from '../../apis/config/axios';
-// import * as env from '../../../env.config';
+import { axiosAuthInstance } from '../../apis/config/axios';
+import * as env from '../../../env.config';
 
 const KakaoLogin = () => {
   const navigate = useNavigate();
@@ -17,17 +17,18 @@ const KakaoLogin = () => {
         }
         // 백엔드 서버로 인가 코드 보내서 서버에서 http 요청 보내기.
         //cors.
-        // const uri = 'https://kauth.kakao.com/oauth/token';
+        const uri = 'https://kauth.kakao.com/oauth/token';
 
-        // const requestBody = {
-        //   grant_type: 'authorization_code',
-        //   client_id: env.VITE_APP_REST_API_KEY,
-        //   redirect_uri: env.VITE_APP_REDIRECT_URL,
-        //   code: code,
-        //   client_secret: env.VITE_APP_CLIENT_SECRET,
-        // };
+        const payload = {
+          grant_type: 'authorization_code',
+          client_id: env.VITE_APP_REST_API_KEY,
+          redirect_uri: env.VITE_APP_REDIRECT_URL,
+          code: code,
+          client_secret: env.VITE_APP_CLIENT_SECRET,
+        };
 
-        // const response = await axiosAuthInstance.post(uri, requestBody);
+        const response = await axiosAuthInstance.post(uri, payload);
+        console.log(response);
       } catch (error) {
         alert(error.message);
         navigate('/login');
