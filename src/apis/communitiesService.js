7cef354e34d521af994dd7a1561b7e3235d527c6
jyj1111 <jyj202040@gmail.com>
2023-07-18 20:48:32 +0900

    feat: 독후감, 도서 검색 api추가

--- src/apis/communitiesService.js
+++ src/apis/communitiesService.js
@@ -1,8 +1,11 @@
 import { axiosJsonInstance } from './config/axios';
 
-export const getAllCommnunities = async () => {
+export const freeRegister = async (title, content) => {
   try {
-    const { data } = await axiosJsonInstance.get(`/communities`);
+    const { data } = await axiosJsonInstance.post(`/communities/board/free`, {
+      title,
+      content,
+    });
     console.log(data);
     return data;
   } catch (error) {
@@ -10,11 +13,22 @@ export const getAllCommnunities = async () => {
   }
 };
 
-export const freeRegister = async (title, content) => {
+export const reportRegister = async (
+  title,
+  content,
+  book_title,
+  authors,
+  publisher,
+  pud_date
+) => {
   try {
-    const { data } = await axiosJsonInstance.post(`/communities/board/free`, {
+    const { data } = await axiosJsonInstance.post(`/communities/board/report`, {
       title,
-      content,
+      contents: content,
+      book_title,
+      authors,
+      publisher,
+      pud_date,
     });
     console.log(data);
     return data;
@@ -35,6 +49,21 @@ export const bookClubRegister = async (title, content, location) => {
     );
     console.log(data, status);
     return { data, status };
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+export const bookSearch = async (book_title) => {
+  try {
+    const { data: data1, status } = await axiosJsonInstance.get(
+      `/books/aladdin/search?query=${book_title}`
+    );
+
+    const { data: data2 } = data1;
+    const { item } = data2;
+    console.log(item, status);
+    return { item, status };
   } catch (error) {
     console.log(error.response);
   }
