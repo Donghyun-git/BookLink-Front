c3983b765d2d1ac91dca3867ea461a02b03d425e
jyj1111 <jyj202040@gmail.com>
2023-07-21 23:48:29 +0900

    feat: 게시판 리스트

--- src/components/Communities/Boards/BoardsForm.jsx
+++ src/components/Communities/Boards/BoardsForm.jsx
@@ -1,14 +1,44 @@
 //import React from 'react'
 import * as Styled from './Styled';
+import { frees, bookReports } from '../../../lib/apis/communitiesService';
+import { useState, useEffect } from 'react';
 const BoardsForm = () => {
+  const [freeList, setFreeList] = useState([]);
+  const [bookReportList, setBookReportList] = useState([]);
+  const [select, setSelect] = useState(0); //상태가 0일시 자유글, 상태가 1일시 독후감
+  const getFrees = async () => {
+    const { data } = await frees();
+    setFreeList(data);
+  };
+  const getBookReports = async () => {
+    const { data } = await bookReports();
+    setBookReportList(data);
+  };
+  useEffect(() => {
+    getFrees();
+    getBookReports();
+  }, []);
+  const dateFormat = (localDate) => {
+    let [date, time] = localDate.split('T');
+    time = time.split(':').slice(0, 2).join(':');
+    return date + ' ' + time;
+  };
+
+  const onSelectHandler = (e) => {
+    if (e.target.value === '자유글') {
+      setSelect(0);
+    } else if (e.target.value === '독후감') {
+      setSelect(1);
+    }
+  };
+
   return (
     <Styled.MainContainerDiv>
       <Styled.MainContentsDiv>
         <h2>📖게시판</h2>
         <Styled.SelectDiv>
           <div>
-            <select>
-              <option value="전체보기">전체보기</option>
+            <select onChange={onSelectHandler}>
               <option value="자유글">자유글</option>
               <option value="독후감">독후감</option>
             </select>
@@ -16,30 +46,25 @@ const BoardsForm = () => {
           <input type="search" placeholder="검색어를 입력해보세요" />
         </Styled.SelectDiv>
         <Styled.ContentsDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
-          <Styled.CardDiv></Styled.CardDiv>
+          {select
+            ? bookReportList.map(({ lastModifiedTime, title, content }) => {
+                return (
+                  <Styled.CardDiv key={lastModifiedTime}>
+                    <p>{dateFormat(lastModifiedTime)}</p>
+                    <p>{title}</p>
+                    <p>{content}</p>
+                  </Styled.CardDiv>
+                );
+              })
+            : freeList.map(({ lastModifiedTime, title, content }) => {
+                return (
+                  <Styled.CardDiv key={lastModifiedTime}>
+                    <p>{dateFormat(lastModifiedTime)}</p>
+                    <p>{title}</p>
+                    <p>{content}</p>
+                  </Styled.CardDiv>
+                );
+              })}
         </Styled.ContentsDiv>
       </Styled.MainContentsDiv>
     </Styled.MainContainerDiv>
