29da950ff01e78adfe2b17ac38d5bf7da4c65277
jyj1111 <jyj202040@gmail.com>
2023-07-24 22:25:38 +0900

    feat: 독후감 상세페이지 추가

---
+++ src/pages/BookReportDetail/index.jsx
@@ -0,0 +1,11 @@
+import BookReportDetailForm from '../../components/Communities/Boards/BookReportDetail';
+
+const BookReportDetailPage = () => {
+  return (
+    <div>
+      <BookReportDetailForm />
+    </div>
+  );
+};
+
+export default BookReportDetailPage;
