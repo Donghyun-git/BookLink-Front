7b4011b0565f6a0835c945d97d29af89556a81d5
jyj1111 <jyj202040@gmail.com>
2023-07-24 22:26:07 +0900

    feat: 자유글 상세 페이지 추가

---
+++ src/pages/FreeReportDetail/index.jsx
@@ -0,0 +1,11 @@
+//import React from 'react'
+import FreeReportDetailForm from '../../components/Communities/Boards/FreeReportDetail/FreeReportDetailForm';
+const FreeReportDetailPage = () => {
+  return (
+    <div>
+      <FreeReportDetailForm />
+    </div>
+  );
+};
+
+export default FreeReportDetailPage;
