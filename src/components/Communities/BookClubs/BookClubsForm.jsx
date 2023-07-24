1e39aba893d1380653313056699a1e2faffa2c5f
jyj1111 <jyj202040@gmail.com>
2023-07-24 22:25:04 +0900

    feat: 도서모집 상세페이지 추가

--- src/components/Communities/BookClubs/BookClubsForm.jsx
+++ src/components/Communities/BookClubs/BookClubsForm.jsx
@@ -1,20 +1,30 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import * as Styled from './Styled';
 import { bookClubList } from '../../../lib/apis/communitiesService';
 import bell from '../../../images/bell.png';
+import BookClubsCardForm from '../../Common/Card/BookClubsCard/BookClubsCardForm';
 const BookClubsForm = () => {
   const [bookClubs, setBookClubs] = useState([]);
+  const [select, setSelect] = useState('전체보기');
+  const inputRef = useRef(null);
   const getBookClubs = async () => {
-    const { data } = await bookClubList();
+    let { data } = await bookClubList();
+    if (select && select !== '전체보기') {
+      data = data.filter((info) => info.location.split(' ')[0] === select);
+    }
     setBookClubs(data);
   };
   useEffect(() => {
     getBookClubs();
-  }, []);
-  const dateFormat = (localDate) => {
-    let [date, time] = localDate.split('T');
-    time = time.split(':').slice(0, 2).join(':');
-    return date + ' ' + time;
+  }, [select]);
+  const onSelectHandler = (e) => {
+    const { value } = e.target;
+    setSelect(value);
+  };
+  const onSearchHandler = (e) => {
+    if (e.key === 'Enter') {
+      setSelect(inputRef.current.value);
+    }
   };
   return (
     <Styled.MainContainerDiv>
@@ -24,44 +34,31 @@ const BookClubsForm = () => {
           <Styled.tag>독서 모임 모집</Styled.tag>
         </Styled.TagDiv>
         <Styled.SelectDiv>
-          <Styled.select>
+          <Styled.select onChange={onSelectHandler}>
             <option value="전체보기">전체보기</option>
             <option value="서울">서울</option>
             <option value="인천">인천</option>
-            <option value="지역명">지역명</option>
           </Styled.select>
           <Styled.searchInput
             type="search"
             placeholder="독서 모임 위치로 검색해 보세요"
+            ref={inputRef}
+            onKeyDown={onSearchHandler}
           />
         </Styled.SelectDiv>
         <Styled.ContentsDiv>
           {bookClubs.map(
             ({ location, reply_cnt, writer, date, title, content }) => {
               return (
-                <Styled.CardDiv key={date}>
-                  <Styled.CardContainerDiv>
-                    <Styled.CardHeaderDiv>
-                      <p>{location}</p>
-                      <p>{reply_cnt}</p>
-                    </Styled.CardHeaderDiv>
-                    <Styled.CardInfoDiv>
-                      <Styled.CardInfoWriterDiv>
-                        {writer}
-                      </Styled.CardInfoWriterDiv>
-                      <Styled.CardInfoCategoryDiv>
-                        독서모임
-                      </Styled.CardInfoCategoryDiv>
-                      <Styled.CardInfoDateDiv>
-                        {dateFormat(date)}
-                      </Styled.CardInfoDateDiv>
-                    </Styled.CardInfoDiv>
-                    <Styled.CardTitleDiv>{title}</Styled.CardTitleDiv>
-                    <Styled.CardContentDiv
-                      dangerouslySetInnerHTML={{ __html: content }}
-                    />
-                  </Styled.CardContainerDiv>
-                </Styled.CardDiv>
+                <BookClubsCardForm
+                  key={date}
+                  location={location}
+                  reply_cnt={reply_cnt}
+                  writer={writer}
+                  date={date}
+                  title={title}
+                  content={content}
+                />
               );
             }
           )}
