1c2b346f75a057af0724f5e769b79716f37b878b
jyj1111 <jyj202040@gmail.com>
2023-07-28 15:44:19 +0900

    Refactor: 커뮤니티 수정, 삭제 api 리팩토링

---
+++ src/lib/apis/communities/delete/communitiesDeleteService.js
@@ -0,0 +1,40 @@
+import { axiosJsonInstance } from '../../config/axios';
+
+export const bookClubsDelete = async (id) => {
+  try {
+    const { data: data1, status } = await axiosJsonInstance.delete(
+      `/communities/book-club/${id}`
+    );
+    const { data } = data1;
+    console.log(data);
+    return { data, status };
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+export const freesDelete = async (id) => {
+  try {
+    const { data: data1, status } = await axiosJsonInstance.delete(
+      `/communities/board/free/${id}`
+    );
+    const { data } = data1;
+    console.log(data);
+    return { data, status };
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+export const bookReportsDelete = async (id) => {
+  try {
+    const { data: data1, status } = await axiosJsonInstance.delete(
+      `/communities/board/report/${id}`
+    );
+    const { data } = data1;
+    console.log(data);
+    return { data, status };
+  } catch (error) {
+    console.log(error.response);
+  }
+};
