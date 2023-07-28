1fe54ab05d5f061582341dc04935b86d542d03ba
jyj1111 <jyj202040@gmail.com>
2023-07-28 15:34:36 +0900

    feat: 도시 지역 선택 컴포넌트 생성

--- src/components/Common/CityDistrictSelector/CityDistrictSelectorForm.jsx
+++ src/components/Common/CityDistrictSelector/CityDistrictSelectorForm.jsx
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
-
+import { data } from '../../../constants/sidoguInfo';
+import { useState } from 'react';
+const Tag = styled.div`
+  margin-top: 2.571rem;
+  margin-bottom: 0.571rem;
+  font-size: 1.714rem;
+  font-weight: bold;
+`;
 const CityContainerDiv = styled.div`
   display: flex;
   height: 13.857rem;
 `;
 const CitiesDiv = styled.div`
   width: 10.714rem;
+  border: 1px solid black;
   overflow: auto;
 `;
 const CityDiv = styled.div`
@@ -28,17 +36,42 @@ const DistrictDiv = styled.div`
   height: 1.714rem;
 `;
 
-const CityDistrictSelectorForm = ({
-  cities,
-  onCityHandler,
-  cityActive,
-  districts,
-  onDistrictHandler,
-  districtActive,
-}) => {
+const CityDistrictSelectorForm = ({ city = '', district = '', setValue }) => {
+  const cities = Object.keys(data);
+  const [cityActive, setCityActive] = useState(
+    cities.map((city1) => city1 === city)
+  );
+  const [districts, setDistricts] = useState(city ? data[city] : []);
+  const [districtActive, setDistrictActive] = useState(
+    city && data[city]
+      ? data[city].map((district1) => district1 === district)
+      : []
+  );
+  const onCityHandler = (e) => {
+    if (e.target.innerText) {
+      setValue('city', e.target.innerText);
+      setCityActive(cities.map((city) => city === e.target.innerText));
+      console.log(cityActive);
+      setDistricts(data[e.target.innerText]);
+      console.log(districts);
+      setDistrictActive(new Array(data[e.target.innerText].length).fill(false));
+    }
+  };
+
+  const onDistrictHandler = (e) => {
+    if (e.target.innerText) {
+      setValue('district', e.target.innerText);
+      setDistrictActive(
+        districts.map((district) =>
+          district === e.target.innerText ? true : false
+        )
+      );
+    }
+  };
+
   return (
     <div>
-      <h2>모임위치</h2>
+      <Tag>모임위치</Tag>
       <CityContainerDiv>
         <CitiesDiv>
           {cities.map((city, idx) => {
