b940a36d21fb6f131c00ca581e12c69518fd370e
jyj1111 <jyj202040@gmail.com>
2023-08-02 01:08:47 +0900

    feat: 댓글 삭제, 수정, 좋아요, 대댓글 작성 구현

--- src/components/Common/CommunitiesComment/CommuntiesCommentCard/CommunitiesCommentCardForm.jsx
+++ src/components/Common/CommunitiesComment/CommuntiesCommentCard/CommunitiesCommentCardForm.jsx
@@ -1,109 +1,248 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import * as Styled from './Styled';
 import threePointUrl from '../../../../images/threePoints.png';
 import Thumbs from '../../../../images/thumbs.svg';
 import LikeThumbs from '../../../../images/liked_thumbs.svg';
 import DownToggle from '../../../../images/down_toggle.svg';
-//import UpToggle from '../../../../images/up_toggle.svg';
+import UpToggle from '../../../../images/up_toggle.svg';
 import { getDateDistance } from '../../../../utils/date';
 import {
+  bookClubsCommentRegister,
   bookClubsCommentDelete,
   bookClubsCommentModify,
   bookClubsCommentLike,
+  bookReportCommentRegister,
+  bookReportCommentDelete,
+  bookReportCommentModify,
+  bookReportCommentLike,
+  freeReportCommentRegister,
+  freeReportCommentDelete,
+  freeReportCommentModify,
+  freeReportCommentLike,
 } from '../../../../lib/apis/communities/comment/communitiesCommentService';
+import { CommunitiesDetailContext } from '../../../../context/communitiesDetailContext';
 const CommunitiesCommentCardForm = ({
   replyId,
   image,
   writer,
   date,
   isLiked,
+  /*isUpdated,*/
   content,
   like_cnt,
   sub_reply_cnt,
 }) => {
+  const { info, setInfo } = useContext(CommunitiesDetailContext);
+  const { category, replies, reply_cnt } = info;
   const contentRef = useRef(null);
-  const [contentEditable, setContentEditable] = useState(false);
+  const replyRef = useRef(null);
   const { id } = useParams();
 
+  const [contentEditable, setContentEditable] = useState(false);
+  const [replyStatus, setReplyStatus] = useState(false);
+  const [replyNum, setReplyNum] = useState(sub_reply_cnt);
+  const [toggleStatus, setToggleStatus] = useState(false);
   const [likeStatus, setLikeStatus] = useState(isLiked);
   const [likeNum, setLikeNum] = useState(like_cnt);
 
   const onCommentModify = async (e) => {
+    let data1;
     if (e.key === 'Enter') {
       setContentEditable(!contentEditable);
       const content = contentRef.current.innerText;
-      const { data } = await bookClubsCommentModify(content, id, replyId);
-      console.log(data);
+      if (category === '독서 모임') {
+        const { data } = await bookClubsCommentModify(content, id, replyId);
+        data1 = data;
+      } else if (category === '독후감') {
+        const { data } = await bookReportCommentModify(content, id, replyId);
+        data1 = data;
+      } else {
+        const { data } = await freeReportCommentModify(content, id, replyId);
+        data1 = data;
+      }
+      console.log(data1);
     }
   };
   const onCommentDelete = async () => {
-    const { data } = await bookClubsCommentDelete(id, replyId);
-    const { like_cnt } = data;
-    console.log(like_cnt);
-    setLikeNum(like_cnt);
-    setLikeStatus(!likeStatus);
+    if (category === '독서 모임') {
+      const { status } = await bookClubsCommentDelete(id, replyId);
+      console.log(status);
+    } else if (category === '독후감') {
+      const { status } = await bookReportCommentDelete(id, replyId);
+      console.log(status);
+    } else {
+      const { status } = await freeReportCommentDelete(id, replyId);
+      console.log(status);
+    }
+    const replies = info.replies.filter(({ id }) => id !== replyId);
+    setInfo({ ...info, reply_cnt: reply_cnt - 1 - replyNum, replies });
   };
 
   const onCommentLike = async () => {
-    console.log(id, replyId);
-    const { status } = await bookClubsCommentLike(id, replyId);
-    console.log(status);
+    let data1;
+    if (category === '독서 모임') {
+      const { data } = await bookClubsCommentLike(id, replyId);
+      const { like_cnt } = data;
+      data1 = like_cnt;
+    } else if (category === '독후감') {
+      const { data } = await bookReportCommentLike(id, replyId);
+      const { like_cnt } = data;
+      data1 = like_cnt;
+    } else {
+      const { data } = await freeReportCommentLike(id, replyId);
+      const { like_cnt } = data;
+      data1 = like_cnt;
+    }
+    setLikeNum(data1);
+    setLikeStatus(!likeStatus);
   };
+  const onReplyPost = async (e) => {
+    let data1;
+    if (e.key === 'Enter') {
+      const Content = replyRef.current.value;
+      if (category === '독서 모임') {
+        const { data } = await bookClubsCommentRegister(Content, id, replyId);
+        data1 = data;
+      } else if (category === '독후감') {
+        const { data } = await bookReportCommentRegister(Content, id, replyId);
+        data1 = data;
+      } else {
+        const { data } = await freeReportCommentRegister(Content, id, replyId);
+        data1 = data;
+      }
 
+      const { replyId: subReplyId, date, content, writer, image } = data1;
+      const replies = [
+        {
+          id: subReplyId,
+          parent_id: replyId,
+          writer,
+          content,
+          date,
+          image,
+          like_cnt: 0,
+          sub_reply_cnt: 0,
+          isLiked: false,
+          isUpdated: false,
+        },
+        ...info.replies,
+      ];
+      setReplyNum(replyNum + 1);
+      setInfo({ ...info, reply_cnt: reply_cnt + 1, replies });
+    }
+  };
+  //console.log(replies);
+  console.log(toggleStatus);
   useEffect(() => {
     setLikeNum(like_cnt);
     setLikeStatus(isLiked);
   }, [like_cnt]);
 
   return (
-    <div>
-      <Styled.CommentForm>
-        <Styled.WriterImg>
-          <img src={image} />
-        </Styled.WriterImg>
-        <Styled.CommentInfo>
-          <Styled.CommentInfoHeader>
-            <Styled.CommentInfoHeaderLeft>
-              <span>{writer}</span>
-              <span>{getDateDistance(date)} 전</span>
-            </Styled.CommentInfoHeaderLeft>
-            <Styled.CommentInfoHeaderRight>
-              <div>
-                <Styled.Button
-                  onClick={() => {
-                    setContentEditable(!contentEditable);
-                  }}
-                >
-                  수정
-                </Styled.Button>
-                <Styled.Button onClick={onCommentDelete}>
-                  삭제하기
-                </Styled.Button>
-              </div>
-              <img src={threePointUrl} />
-            </Styled.CommentInfoHeaderRight>
-          </Styled.CommentInfoHeader>
-          <Styled.CommentInfoMain
-            contentEditable={contentEditable}
-            ref={contentRef}
-            onKeyDown={onCommentModify}
-          >
-            {content}
-          </Styled.CommentInfoMain>
-          <Styled.CommentInfoFooter>
-            <img src={isLiked ? LikeThumbs : Thumbs} onClick={onCommentLike} />
+    <Styled.CommentForm>
+      <Styled.WriterImg>
+        <img src={image} />
+      </Styled.WriterImg>
+      <Styled.CommentInfo>
+        <Styled.CommentInfoHeader>
+          <Styled.CommentInfoHeaderLeft>
+            <span>{writer}</span>
+            <span>{getDateDistance(date)} 전</span>
+          </Styled.CommentInfoHeaderLeft>
+          <Styled.CommentInfoHeaderRight>
+            <div>
+              <Styled.Button
+                onClick={() => {
+                  setContentEditable(!contentEditable);
+                }}
+              >
+                수정
+              </Styled.Button>
+              <Styled.Button onClick={onCommentDelete}>삭제하기</Styled.Button>
+            </div>
+            <img src={threePointUrl} />
+          </Styled.CommentInfoHeaderRight>
+        </Styled.CommentInfoHeader>
+        <Styled.CommentInfoMain
+          contentEditable={contentEditable}
+          ref={contentRef}
+          onKeyDown={onCommentModify}
+        >
+          {content}
+        </Styled.CommentInfoMain>
+        <Styled.CommentInfoFooter>
+          <Styled.CommentInfoFooterTop>
+            <img
+              src={likeStatus ? LikeThumbs : Thumbs}
+              onClick={onCommentLike}
+            />
             <span>{likeNum}</span>
-            {sub_reply_cnt > 0 && (
-              <>
-                <img src={DownToggle} />
-                <span>{sub_reply_cnt}</span>
-              </>
-            )}
-          </Styled.CommentInfoFooter>
-        </Styled.CommentInfo>
-      </Styled.CommentForm>
-    </div>
+            <span
+              onClick={() => {
+                setReplyStatus(!replyStatus);
+              }}
+            >
+              답글 달기
+            </span>
+          </Styled.CommentInfoFooterTop>
+          {replyNum > 0 && (
+            <>
+              <img
+                src={toggleStatus ? UpToggle : DownToggle}
+                onClick={() => setToggleStatus(!toggleStatus)}
+              />
+              <span>답글 {replyNum}개</span>
+            </>
+          )}
+          {toggleStatus && (
+            <>
+              {replies
+                .filter(
+                  ({ id, parent_id }) =>
+                    id !== parent_id && parent_id === replyId
+                )
+                .map(
+                  ({
+                    id,
+                    image,
+                    writer,
+                    isLiked,
+                    isUpdated,
+                    date,
+                    content,
+                    like_cnt,
+                    sub_reply_cnt,
+                  }) => {
+                    return (
+                      <CommunitiesCommentCardForm
+                        key={id}
+                        replyId={id}
+                        image={image}
+                        writer={writer}
+                        isLiked={isLiked}
+                        isUpdated={isUpdated}
+                        date={date}
+                        content={content}
+                        like_cnt={like_cnt}
+                        sub_reply_cnt={sub_reply_cnt}
+                      />
+                    );
+                  }
+                )}
+            </>
+          )}
+          {replyStatus && (
+            <Styled.ReplyPostForm>
+              <Styled.ReplyWriterImg>
+                <img src="https://soccerquick.s3.ap-northeast-2.amazonaws.com/1689834239634.png" />
+              </Styled.ReplyWriterImg>
+              <Styled.ReplyInputForm ref={replyRef} onKeyDown={onReplyPost} />
+            </Styled.ReplyPostForm>
+          )}
+        </Styled.CommentInfoFooter>
+      </Styled.CommentInfo>
+    </Styled.CommentForm>
   );
 };
 
--- src/components/Common/CommunitiesComment/CommuntiesCommentCard/Styled.js
+++ src/components/Common/CommunitiesComment/CommuntiesCommentCard/Styled.js
@@ -2,30 +2,31 @@ import styled from 'styled-components';
 
 export const CommentForm = styled.div`
   display: flex;
+  width: 100%;
   margin-bottom: 2.571rem;
 `;
 
 export const WriterImg = styled.div`
-  width: 3.428rem;
+  width: 4.44%;
   height: 100%;
   border-radius: 3.428rem;
-  margin-right: 0.857rem;
+  margin-right: 3.06%;
   img {
     width: 100%;
     height: 100%;
     object-fit: cover;
-    border-radius: 3.428rem;
+    border-radius: 100%;
   }
 `;
 
 export const CommentInfo = styled.div`
-  margin-left: 0.857rem;
+  width: 92.5%;
   font-size: 0.857rem;
 `;
 export const CommentInfoHeader = styled.div`
   display: flex;
+  width: 100%;
   height: 1.714rem;
-  width: 71.357rem;
   justify-content: space-between;
 `;
 export const CommentInfoHeaderLeft = styled.div`
@@ -57,4 +58,36 @@ export const CommentInfoMain = styled.div`
   max-width: 70.214rem;
   max-height: 5.143rem;
 `;
-export const CommentInfoFooter = styled.div``;
+export const CommentInfoFooter = styled.div`
+  width: 100%;
+`;
+export const CommentInfoFooterTop = styled.div`
+  img {
+    height: 1.426rem;
+  }
+  span {
+    margin: 0.571rem 0.571rem 0 0.571rem;
+  }
+  margin-bottom: 1rem;
+`;
+
+export const ReplyPostForm = styled.div`
+  margin-bottom: 2.571rem;
+  height: 2.571rem;
+  width: 100%;
+  display: flex;
+`;
+export const ReplyWriterImg = styled(WriterImg)`
+  width: 2.571rem;
+  border-radius: 2.571rem;
+  margin-right: 0.857rem;
+`;
+
+export const ReplyInputForm = styled.input`
+  box-sizing: border-box;
+  margin-left: 0.571rem;
+  height: 1.428rem;
+  font-size: 0.857rem;
+  width: 68.714rem;
+  border-bottom: 0.143rem solid #848484;
+`;
