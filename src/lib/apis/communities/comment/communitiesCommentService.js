848558cda9c48629cb34399727ac6958bb21737d
jyj1111 <jyj202040@gmail.com>
2023-08-02 00:56:00 +0900

    feat: 독후감, 자유글 댓글 api추가

--- src/lib/apis/communities/comment/communitiesCommentService.js
+++ src/lib/apis/communities/comment/communitiesCommentService.js
@@ -31,11 +31,10 @@ export const bookClubsCommentModify = async (content, id, replyId) => {
 
 export const bookClubsCommentDelete = async (id, replyId) => {
   try {
-    const { data: data1, status } = await axiosJsonInstance.delete(
+    const { data, status } = await axiosJsonInstance.delete(
       `/communities/book-club/${id}/${replyId}`
     );
-    const { data } = data1;
-    console.log(data);
+    console.log(status);
     return { data, status };
   } catch (error) {
     console.log(error.response);
@@ -46,6 +45,116 @@ export const bookClubsCommentLike = async (id, replyId) => {
   try {
     const { data: data1, status } = await axiosJsonInstance.post(
       `/communities/book-club/${id}/${replyId}/like`
+    );
+    console.log(data1);
+    const { data } = data1;
+    console.log(data);
+    return { data, status };
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+export const bookReportCommentRegister = async (content, id, parentId) => {
+  try {
+    const { data: data1, status } = await axiosJsonInstance.post(
+      `/communities/board/report/${id}`,
+      { content, parentId }
+    );
+    const { data } = data1;
+    console.log(data);
+    return { data, status };
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+export const bookReportCommentModify = async (content, id, replyId) => {
+  console.log(content, id, replyId);
+  try {
+    const { data: data1, status } = await axiosJsonInstance.patch(
+      `/communities/board/report/${id}/${replyId}`,
+      { content }
+    );
+    const { data } = data1;
+    console.log(data);
+    return { data, status };
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+export const bookReportCommentDelete = async (id, replyId) => {
+  try {
+    const { data, status } = await axiosJsonInstance.delete(
+      `/communities/board/report/${id}/${replyId}`
+    );
+    console.log(status);
+    return { data, status };
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+export const bookReportCommentLike = async (id, replyId) => {
+  try {
+    const { data: data1, status } = await axiosJsonInstance.post(
+      `/communities/board/report/${id}/${replyId}/like`
+    );
+    console.log(data1);
+    const { data } = data1;
+    console.log(data);
+    return { data, status };
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+export const freeReportCommentRegister = async (content, id, parentId) => {
+  try {
+    const { data: data1, status } = await axiosJsonInstance.post(
+      `/communities/board/free/${id}`,
+      { content, parentId }
+    );
+    const { data } = data1;
+    console.log(data);
+    return { data, status };
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+export const freeReportCommentModify = async (content, id, replyId) => {
+  console.log(content, id, replyId);
+  try {
+    const { data: data1, status } = await axiosJsonInstance.patch(
+      `/communities/board/free/${id}/${replyId}`,
+      { content }
+    );
+    const { data } = data1;
+    console.log(data);
+    return { data, status };
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+export const freeReportCommentDelete = async (id, replyId) => {
+  try {
+    const { data, status } = await axiosJsonInstance.delete(
+      `/communities/board/free/${id}/${replyId}`
+    );
+    console.log(status);
+    return { data, status };
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+export const freeReportCommentLike = async (id, replyId) => {
+  try {
+    const { data: data1, status } = await axiosJsonInstance.post(
+      `/communities/board/free/${id}/${replyId}/like`
     );
     console.log(data1);
     const { data } = data1;
