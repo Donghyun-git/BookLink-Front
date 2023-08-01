7458054ea93db2024fcdd13c93f69ab24507aef2
jyj1111 <jyj202040@gmail.com>
2023-08-02 00:56:46 +0900

    feat: 컨텍스트 폴더 생성 및 커뮤니티상세페이지context생성

---
+++ src/context/communitiesDetailContext.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+export const CommunitiesDetailContext = createContext(null);
+//export const BookClubsDetailContext = createContext(null);
+//export const BookReportsDetailContext = createContext(null);
+//export const FreeReportsDetailContext = createContext(null);
