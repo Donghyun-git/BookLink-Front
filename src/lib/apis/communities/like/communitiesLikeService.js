31c8808ec4a444c68a20652a970c50d5009d7f38
jyj1111 <jyj202040@gmail.com>
2023-08-02 00:55:29 +0900

    feat: 독후감, 자유글 좋아요 api추가

--- src/lib/apis/communities/like/communitiesLikeService.js
+++ src/lib/apis/communities/like/communitiesLikeService.js
@@ -12,3 +12,29 @@ export const bookClubLike = async (id) => {
     console.log(error.response);
   }
 };
+
+export const bookReportLike = async (id) => {
+  try {
+    const { data: data1, status } = await axiosJsonInstance.post(
+      `/communities/board/report/${id}/like`
+    );
+    const { data } = data1;
+    console.log(data);
+    return { data, status };
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+export const freeReportLike = async (id) => {
+  try {
+    const { data: data1, status } = await axiosJsonInstance.post(
+      `/communities/board/free/${id}/like`
+    );
+    const { data } = data1;
+    console.log(data);
+    return { data, status };
+  } catch (error) {
+    console.log(error.response);
+  }
+};
