3d31ea2d7a7a5855358bfa0826704e59f05fb2c1
donghyun-git <donghyun1113@hanmail.net>
2023-08-01 22:05:21 +0900

    fix: redux로 리팩토링

--- src/redux/reducers/bookReducer.js
+++ src/redux/reducers/bookReducer.js
@@ -1,6 +1,9 @@
 const initialState = {
+  initial: [],
   books: [],
-  isLoading: false,
+  isCurrent: true,
+  isLikes: false,
+  isLoading: true,
   error: null,
 };
 
@@ -14,28 +17,64 @@ export const bookReducer = (state = initialState, action) => {
     }
     case 'BOOKS/FILTER': {
       return {
-        books: action.payload.payload,
+        ...state,
+        initial: action.payload.books,
+        books: action.payload.books,
         isLoading: false,
       };
     }
     case 'BOOKS/SUCCESS': {
       return {
-        books: action.payload.payload,
+        ...state,
+        initial: action.payload.books,
+        books: action.payload.books,
         isLoading: false,
+        isCurrent: true,
+        isLikes: false,
       };
     }
     case 'BOOKS/FAILURE': {
       return {
         ...state,
         isLoading: false,
-        error: action.payload,
+        error: action.payload.error,
       };
     }
 
     case 'BOOKS/SEARCH': {
       return {
-        books: action.payload.payload,
+        ...state,
+        initial: action.payload.search,
+        books: action.payload.search,
         isLoading: false,
+      };
+    }
+
+    case 'BOOKS/SORT_CURRENT': {
+      return {
+        ...state,
+        isCurrent: true,
+        isLikes: false,
+      };
+    }
+
+    case 'BOOKS/SORT_LIKES': {
+      const sortedLikes = [...state.books].sort((a, b) => {
+        return b.like_cnt - a.like_cnt;
+      });
+
+      return {
+        ...state,
+        books: sortedLikes,
+        isCurrent: false,
+        isLikes: true,
+      };
+    }
+
+    case 'ERROR': {
+      return {
+        ...state,
+        error: true,
       };
     }
 
