c0a89987ae4880e05e3378b9bfe5b9df23c471b5
jyj1111 <jyj202040@gmail.com>
2023-08-08 23:30:15 +0900

    style: 기본 색깔, 폰트굵기 전역으로 공급

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import '../public/css/default.css';
-
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 import Router from './routes/Router';
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Router />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.js
@@ -0,0 +1,34 @@
+const color = {
+  white: '#FFFFFF',
+  lightGreen: '#00AB61',
+  middleGreen: '#00AB61',
+  primaryGreen: '#00E37D',
+  primaryBlue: '#003C74',
+  darkBlue: '#00162C',
+  lightGray: '#D9D9D9',
+  middleGray: '#767676',
+  black: '#000000',
+};
+
+const fontWeight = {
+  normal: '400',
+  regular: '500',
+  bold: '700',
+};
+/*
+  --dark-gray: #444444;
+    --super-light-gray: #FAFBFA;
+  --bg-gray: #FAFBFA;
+    
+  --state-blue: #0750C9;
+  --state-blue-light: #3365FD;
+  --state-red: #E13211;
+  --state-green: #089800;
+  --state-orange: #EE7101;
+*/
+const theme = {
+  color,
+  fontWeight,
+};
+
+export default theme;
