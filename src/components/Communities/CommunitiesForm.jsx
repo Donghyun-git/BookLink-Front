7023b369e5b866b7c64759855d661697e8ba92b1
jyj1111 <jyj202040@gmail.com>
2023-08-10 17:16:52 +0900

    feat: 커뮤니티 홈 error, loading, data 선언형으로 처리

--- src/components/Communities/CommunitiesForm.jsx
+++ src/components/Communities/CommunitiesForm.jsx
@@ -1,100 +1,93 @@
 import * as Styled from './Styled';
-import { useState, useEffect, Suspense } from 'react';
+
+import { useQuery } from 'react-query';
+
 import {
   bookClubList,
-  frees,
+  freeReports,
   bookReports,
 } from '../../lib/apis/communities/communitiesService';
+
 import fire from '../../images/fire.png';
 import bell from '../../images/bell.png';
 import openbook from '../../images/openbook.png';
+
 import BookClubsCardForm from '../Common/Card/BookClubsCard/BookClubsCardForm';
 import BoardsCardForm from '../Common/Card/BoardsCard/BoardsCardForm';
+
 import moment from 'moment';
-import Loading from '../Loading/Loading';
+
 const CommunitiesForm = () => {
-  const [boardList, setBoardList] = useState([]);
-  const [bookClubs, setBookClubs] = useState([]);
-
-  const getList = async () => {
-    const [freeData, bookReportData, bookClubData] = await Promise.all([
-      frees(),
-      bookReports(),
-      bookClubList(),
-    ]);
-
-    setBoardList([...freeData.data, ...bookReportData.data]);
-    setBookClubs(bookClubData.data.slice(0, 4));
-  };
-
-  useEffect(() => {
-    getList();
-  }, []);
+  const { data: bookClubData } = useQuery('bookclubs', bookClubList);
+  const { data: bookData } = useQuery('books', bookReports);
+  const { data: freeData } = useQuery('frees', freeReports);
+  const boardData = [...bookData, ...freeData];
 
   return (
-    <Suspense fallback={<Loading />}>
-      <Styled.MainContainerDiv>
-        <Styled.MainContentsDiv>
-          <Styled.PopularDiv>
-            <Styled.TagDiv>
-              <Styled.fireImg src={fire} alt="불" />
-              <Styled.tag>주간 인기글</Styled.tag>
-              <Styled.week>
-                {moment().startOf('week').format('MM.DD')}~
-                {moment().endOf('week').format('MM.DD')}
-              </Styled.week>
-              {/*<Styled.link href="/communities/popular">전체보기</Styled.link>*/}
-            </Styled.TagDiv>
-            <Styled.PopularContentSDiv>
-              {boardList
-                .filter(
-                  (date) =>
-                    Date.parse(moment().startOf('week')) <=
-                      Date.parse(date.localDateTime) &&
-                    Date.parse(date.localDateTime) <=
-                      Date.parse(moment().endOf('week'))
-                )
-                .slice(0, 4)
-                .sort(
-                  (a, b) =>
-                    b.view_cnt + 2 * b.like_cnt - (a.view_cnt + 2 * a.like_cnt)
-                )
-                .map(
-                  ({
-                    id,
-                    writer,
-                    category,
-                    like_cnt,
-                    reply_cnt,
-                    localDateTime,
-                    title,
-                    content,
-                  }) => {
-                    return (
-                      <BoardsCardForm
-                        key={localDateTime}
-                        category={category}
-                        writer={writer}
-                        like_cnt={like_cnt}
-                        reply_cnt={reply_cnt}
-                        localDateTime={localDateTime}
-                        title={title}
-                        content={content}
-                        id={id}
-                      />
-                    );
-                  }
-                )}
-            </Styled.PopularContentSDiv>
-          </Styled.PopularDiv>
-          <Styled.BookClubsDiv>
-            <Styled.TagDiv>
-              <Styled.bellImg src={bell} alt="벨" />
-              <Styled.tag>독서 모임 모집</Styled.tag>
-              <Styled.link href="/communities/book-clubs">전체보기</Styled.link>
-            </Styled.TagDiv>
-            <Styled.BookClubsContentSDiv>
-              {bookClubs.map(
+    <Styled.MainContainerDiv>
+      <Styled.MainContentsDiv>
+        <Styled.PopularDiv>
+          <Styled.TagDiv>
+            <Styled.fireImg src={fire} alt="불" />
+            <Styled.tag>주간 인기글</Styled.tag>
+            <Styled.week>
+              {moment().startOf('week').format('MM.DD')}~
+              {moment().endOf('week').format('MM.DD')}
+            </Styled.week>
+            {/*<Styled.link href="/communities/popular">전체보기</Styled.link>*/}
+          </Styled.TagDiv>
+          <Styled.PopularContentSDiv>
+            {boardData
+              .filter(
+                (date) =>
+                  Date.parse(moment().startOf('week')) <=
+                    Date.parse(date.localDateTime) &&
+                  Date.parse(date.localDateTime) <=
+                    Date.parse(moment().endOf('week'))
+              )
+              .slice(0, 4)
+              .sort(
+                (a, b) =>
+                  b.view_cnt + 2 * b.like_cnt - (a.view_cnt + 2 * a.like_cnt)
+              )
+              .map(
+                ({
+                  id,
+                  writer,
+                  category,
+                  like_cnt,
+                  reply_cnt,
+                  localDateTime,
+                  title,
+                  content,
+                }) => {
+                  return (
+                    <BoardsCardForm
+                      key={localDateTime}
+                      category={category}
+                      writer={writer}
+                      like_cnt={like_cnt}
+                      reply_cnt={reply_cnt}
+                      localDateTime={localDateTime}
+                      title={title}
+                      content={content}
+                      id={id}
+                    />
+                  );
+                }
+              )}
+          </Styled.PopularContentSDiv>
+        </Styled.PopularDiv>
+        <Styled.BookClubsDiv>
+          <Styled.TagDiv>
+            <Styled.bellImg src={bell} alt="벨" />
+            <Styled.tag>독서 모임 모집</Styled.tag>
+            <Styled.link href="/communities/book-clubs">전체보기</Styled.link>
+          </Styled.TagDiv>
+          <Styled.BookClubsContentSDiv>
+            {bookClubData
+              .slice(0, 4)
+              .map(
                 ({
                   id,
                   location,
@@ -120,51 +113,50 @@ const CommunitiesForm = () => {
                   );
                 }
               )}
-            </Styled.BookClubsContentSDiv>
-          </Styled.BookClubsDiv>
-          <Styled.BoardsDiv>
-            <Styled.TagDiv>
-              <Styled.openbookImg src={openbook} alt="책" />
-              <Styled.tag>게시판</Styled.tag>
-              <Styled.link href="/communities/boards">전체보기</Styled.link>
-            </Styled.TagDiv>
-            <Styled.BoardsContentsDiv>
-              {boardList
-                .sort(
-                  (a, b) =>
-                    Date.parse(b.localDateTime) - Date.parse(a.localDateTime)
-                )
-                .map(
-                  ({
-                    id,
-                    writer,
-                    category,
-                    like_cnt,
-                    reply_cnt,
-                    localDateTime,
-                    title,
-                    content,
-                  }) => {
-                    return (
-                      <BoardsCardForm
-                        key={localDateTime}
-                        category={category}
-                        writer={writer}
-                        like_cnt={like_cnt}
-                        reply_cnt={reply_cnt}
-                        localDateTime={localDateTime}
-                        title={title}
-                        content={content}
-                        id={id}
-                      />
-                    );
-                  }
-                )}
-            </Styled.BoardsContentsDiv>
-          </Styled.BoardsDiv>
-        </Styled.MainContentsDiv>
-      </Styled.MainContainerDiv>
-    </Suspense>
+          </Styled.BookClubsContentSDiv>
+        </Styled.BookClubsDiv>
+        <Styled.BoardsDiv>
+          <Styled.TagDiv>
+            <Styled.openbookImg src={openbook} alt="책" />
+            <Styled.tag>게시판</Styled.tag>
+            <Styled.link href="/communities/boards">전체보기</Styled.link>
+          </Styled.TagDiv>
+          <Styled.BoardsContentsDiv>
+            {boardData
+              .sort(
+                (a, b) =>
+                  Date.parse(b.localDateTime) - Date.parse(a.localDateTime)
+              )
+              .map(
+                ({
+                  id,
+                  writer,
+                  category,
+                  like_cnt,
+                  reply_cnt,
+                  localDateTime,
+                  title,
+                  content,
+                }) => {
+                  return (
+                    <BoardsCardForm
+                      key={localDateTime}
+                      category={category}
+                      writer={writer}
+                      like_cnt={like_cnt}
+                      reply_cnt={reply_cnt}
+                      localDateTime={localDateTime}
+                      title={title}
+                      content={content}
+                      id={id}
+                    />
+                  );
+                }
+              )}
+          </Styled.BoardsContentsDiv>
+        </Styled.BoardsDiv>
+      </Styled.MainContentsDiv>
+    </Styled.MainContainerDiv>
   );
 };
 
--- src/pages/Communities/index.jsx
+++ src/pages/Communities/index.jsx
@@ -1,6 +1,19 @@
-//import React from 'react'
-import CommunitiesForm from '../../components/Communities/CommunitiesForm';
+import { Suspense, lazy } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import Loading from '../../components/Loading/Loading';
+
+const CommunitiesForm = lazy(() =>
+  import('../../components/Communities/CommunitiesForm')
+);
+
 const CommunitiesPage = () => {
-  return <CommunitiesForm />;
+  return (
+    <ErrorBoundary fallback={<h2>에러</h2>}>
+      <Suspense fallback={<Loading />}>
+        <CommunitiesForm />
+      </Suspense>
+    </ErrorBoundary>
+  );
 };
 export default CommunitiesPage;
