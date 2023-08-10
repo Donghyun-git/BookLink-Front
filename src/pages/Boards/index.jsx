3e9f391e93d38e5df49251d29a62f575261227a7
jyj1111 <jyj202040@gmail.com>
2023-08-10 17:18:23 +0900

    refactor: 게시판 홈페이지 error,loading, data 선언형으로 처리

--- src/pages/Boards/index.jsx
+++ src/pages/Boards/index.jsx
@@ -1,7 +1,20 @@
-//import React from 'react'
-import BoardsForm from '../../components/Communities/Boards/BoardsForm';
+import { Suspense, lazy } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import Loading from '../../components/Loading/Loading';
+
+const BoardsForm = lazy(() =>
+  import('../../components/Communities/Boards/BoardsForm')
+);
+
 const BoardsPage = () => {
-  return <BoardsForm />;
+  return (
+    <ErrorBoundary fallback={<h2>에러</h2>}>
+      <Suspense fallback={<Loading />}>
+        <BoardsForm />
+      </Suspense>
+    </ErrorBoundary>
+  );
 };
 
 export default BoardsPage;
