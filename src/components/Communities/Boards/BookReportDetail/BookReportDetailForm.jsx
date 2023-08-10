e584894ef9d7b307b3910ea1cab67c41b1cbeeaf
jyj1111 <jyj202040@gmail.com>
2023-08-10 17:20:41 +0900

    refactor: 독후감 상세페이지 error,loading, data 선언형으로 처리

--- src/components/Communities/Boards/BookReportDetail/BookReportDetailForm.jsx
+++ src/components/Communities/Boards/BookReportDetail/BookReportDetailForm.jsx
@@ -1,33 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+
 import { bookReportsDetail } from '../../../../lib/apis/communities/detail/communitiesDetailService';
-import { dateFormat } from '../../../../utils/date';
+
 import { useParams } from 'react-router-dom';
+
 import {
   MainContainerDiv,
   CommunitiesDetailContentsDiv,
 } from '../../../../styles/globalStyled';
 import LikeShareForm from '../../../Common/LikeShare/LikeShareForm';
+
 import CommunitiesDetailForm from '../../../Common/CommunitiesDetail/CommunitiesDetailForm';
 import CommunitiesCommentForm from '../../../Common/CommunitiesComment/CommunitiesCommentForm';
-import { CommunitiesDetailContext } from '../../../../context/communitiesDetailContext';
-const BookReportDetailForm = () => {
-  const [info, setInfo] = useState({});
-  const { id } = useParams();
-  const getDetail = async () => {
-    const { data } = await bookReportsDetail(Number(id));
-    console.log(data);
-    const { date, cover, book_title, authors, publisher, pud_date, ...data1 } =
-      data;
 
-    setInfo({
-      ...data1,
-      date: dateFormat(date),
-      bookInfo: { cover, book_title, authors, publisher, pud_date },
-    });
-  };
-  useEffect(() => {
-    getDetail();
-  }, []);
+import { CommunitiesDetailContext } from '../../../../context/communitiesDetailContext';
+
+import { useQuery } from 'react-query';
+
+const BookReportDetailForm = () => {
+  const { id } = useParams();
+  const { data } = useQuery(['books', id], () => bookReportsDetail(Number(id)));
+  const [info, setInfo] = useState(data);
   /*const {
     writer,
     title,
