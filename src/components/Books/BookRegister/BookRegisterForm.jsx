9f1a9c8c4319dea060cd8a4f090ac23619b9a50f
jyj1111 <jyj202040@gmail.com>
2023-08-11 23:55:54 +0900

    style: 책 등록페이지 스타일링

--- src/components/Books/BookRegister/BookRegisterForm.jsx
+++ src/components/Books/BookRegister/BookRegisterForm.jsx
@@ -8,7 +8,28 @@ import BookSearchForm from '../../Common/Search/BookSearch/BookSearchForm';
 import { useParams } from 'react-router-dom';
 const BookRegisterForm = () => {
   const { isbn } = useParams();
-
+  /*{
+    "title": "불편한 편의점 2 (단풍 에디션)",
+    "author": "김호연 (지은이)",
+    "pud_date": "2022-08-10",
+    "isbn13": "9791161571188",
+    "price_sales": 12600,
+    "cover": "https://image.aladin.co.kr/product/29045/74/cover/k192836746_2.jpg",
+    "category_name": "국내도서>소설/시/희곡",
+    "publisher": "나무옆의자",
+    "recommendation": "출간 후 1년이 넘도록 독자의 사랑을 받으며 베스트셀러 상위권을 지키고 있는 소설, 김호연 작가의 『불편한 편의점』이 그 두 번째 이야기로 다시 찾아왔다. 청파동 골목의 작은 편의점을 무대로 힘겨운 시대를 살아가는 우리 이웃들의 삶을 따뜻하고 유쾌하게 그렸다.",
+    "rent_signal": "True",
+    "book_rating": "최상급",
+    "book_status": "새책처럼 깨끗합니다.",
+    "rental_fee": 2500,
+    "min_date": 7,
+    "max_date": 21,
+    "rent_location": "서울특별시 영등포구 신길동",
+    "rent_method": "만나서 현금으로 주시면 됩니다~~~"
+    
+    }
+    사용자가 대여 신청 가능하게 설정할 경우 rent_signal을 True
+    사용자가 대여 신청 불가하게 설정할 경우 rent_signal을 False*/
   return (
     <MainContainerDiv>
       <MainContentsDiv>
@@ -16,10 +37,61 @@ const BookRegisterForm = () => {
         <BookSearchForm />
         <Styled.Tag>추천사 작성하기</Styled.Tag>
         <Styled.Recommend placeholder="회원님만의 추천사를 작성해보세요" />
+        <Styled.RegisterChoice>
+          <Styled.Tag>책을 소장하고 계신가요?</Styled.Tag>
+          <p>
+            대여 신청 받기를 선택하고 대여 정보를 입력하시면 이 책이 필요한
+            이용자에게 대여료를 받고 책을 대여해 줄 수 있습니다.
+          </p>
+          <p>
+            대여 정보를 입력하고 내 주변에서 책이 필요한 사람들에게 책을 대여해
+            보세요!
+          </p>
+          <Styled.Choice>
+            <Styled.Card>
+              <p>대여신청받기</p>
+              <p>다른 이용자가 대여료를 지불하고 대여를 신청할 수 있습니다.</p>
+            </Styled.Card>
+            <Styled.Card>
+              <p>대여신청받지않기</p>
+              <p>다른 이용자에게 대여 신청을 받을 수 없습니다.</p>
+            </Styled.Card>
+          </Styled.Choice>
+        </Styled.RegisterChoice>
+        <Styled.Tag>도서 품질</Styled.Tag>
+        <Styled.Choice>
+          <Styled.Card></Styled.Card>
+          <Styled.Card></Styled.Card>
+          <Styled.Card></Styled.Card>
+          <Styled.Card></Styled.Card>
+          <Styled.Card></Styled.Card>
+          <Styled.Card></Styled.Card>
+        </Styled.Choice>
+        <Styled.Tag>도서 참고사진 등록하기</Styled.Tag>
+        <Styled.PhotoBtn for="img">사진 등록하기</Styled.PhotoBtn>
+        <Styled.PhotoInput type="file" multiple name="img" id="img" />
+        <Styled.Upload></Styled.Upload>
         <Styled.Tag>도서 상태 설명하기</Styled.Tag>
         <Styled.Contents placeholder="도서의 구매일자, 상태를 설명해주세요" />
+        <Styled.RentalFee>
+          <Styled.Tag>대여료</Styled.Tag>
+          <Styled.RentalPriceInput />
+        </Styled.RentalFee>
+        <Styled.RentalTime>
+          <Styled.Tag>대여기간</Styled.Tag>
+          <Styled.Select>
+            <option>14일</option>
+          </Styled.Select>
+          <Styled.Select>
+            <option>3개월</option>
+          </Styled.Select>
+        </Styled.RentalTime>
+        <Styled.Tag>대여장소</Styled.Tag>
+        <Styled.Address />
+        <button type="button">변경하기</button>
         <Styled.Tag>대여방법 작성</Styled.Tag>
         <Styled.Contents placeholder="다른 사람들이 도서를 대여할 수 있도록 대여 안내 글을 작성해 보세요! 상세히 작성할수록 대여를 보다 원활히 진행할 수 있습니다." />
+        <Styled.RegisterBtn type="submit">도서 기록하기</Styled.RegisterBtn>
       </MainContentsDiv>
     </MainContainerDiv>
   );
--- src/components/Books/BookRegister/Styled.js
+++ src/components/Books/BookRegister/Styled.js
@@ -7,6 +7,19 @@ export const MainTag = styled.div`
   font-size: 2.571rem;
   font-weight: ${({ theme }) => theme.fontWeight.regular};
 `;
+export const RegisterChoice = styled.div`
+  width: 49.214rem;
+  height: 15.286rem;
+`;
+export const Choice = styled.ul`
+  display: flex;
+`;
+export const Card = styled.li`
+  width: 15.714rem;
+  height: 6.786rem;
+  margin-right: 1.714rem;
+  border: 2px solid #d9d9d9;
+`;
 export const Contents = styled.textarea`
   margin-top: 0.857rem;
   width: 100%;
@@ -16,9 +29,60 @@ export const Contents = styled.textarea`
   border: 0.143rem solid ${({ theme }) => theme.color.lightGray};
 `;
 export const Tag = styled.div`
+  margin-top: 4.286rem;
   font-size: 1.714rem;
   font-weight: ${({ theme }) => theme.fontWeight.regular};
 `;
+export const PhotoBtn = styled.label`
+  width: 94px;
+  height: 36px;
+  border: 1px solid #d9d9d9;
+  border-radius: 12px;
+`;
+export const PhotoInput = styled.input`
+  display: none;
+`;
+export const Upload = styled.div`
+  width: 100%;
+  height: 10rem;
+  border: 1px solid #d9d9d9;
+`;
+export const RentalFee = styled.div`
+  display: flex;
+`;
+export const RentalPriceInput = styled.input`
+  margin-top: 3.429rem;
+  margin-left: 2.5rem;
+  border: 1px solid #d9d9d9;
+  width: 18.429rem;
+  height: 4.286rem;
+`;
+export const RentalTime = styled.div`
+  display: flex;
+`;
+export const Select = styled.select`
+  margin-top: 3.429rem;
+  width: 10rem;
+  height: 4.286rem;
+`;
+export const Address = styled.input`
+  width: 42.587rem;
+  height: 4.286rem;
+  background: #ffffff;
+  border: 1px solid #d9d9d9;
+  border-radius: 1.714rem;
+`;
 export const Recommend = styled(Contents)`
   height: 8.571rem;
+`;
+export const RegisterBtn = styled.button`
+  margin: 4.286rem 30rem;
+  width: 42.587rem;
+  height: 6rem;
+  background: #00e37d;
+  border-radius: 1.714rem;
+  font-weight: bold;
+  font-size: 1.714rem;
+  text-align: center;
+  color: #ffffff;
 `;
