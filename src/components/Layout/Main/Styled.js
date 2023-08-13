7e486db7d5a808522a8354ad21949a4b0e6f353b
donghyun-git <donghyun1113@hanmail.net>
2023-08-13 16:06:13 +0900

    feat: 메인 페이지 UI 작업

---
+++ src/components/Layout/Main/Styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import bannerImage from '../../../images/banner.png';
+
+export const MainWrap = styled.main`
+  width: 100%;
+`;
+
+export const BannerWrap = styled.div`
+  width: 100%;
+  background: url(${bannerImage}) center top / cover no-repeat;
+  padding: 4.29rem 0;
+  /* height: 57.14286rem; */
+`;
+
+export const MainContainerWrap = styled.div`
+  width: 100%;
+`;
+
+export const MainContainer = styled.div`
+  width: 102.85714rem;
+  margin: 0 auto;
+`;
---
+++ src/components/Main/CommunityBanner.jsx
@@ -0,0 +1,22 @@
+import * as Styled from './Styled';
+
+const CommunityBanner = () => {
+  return (
+    <Styled.CommunityBannerDiv>
+      <Styled.CommunityBannerTitleDiv>
+        <h2>사람들과 함께 독서를 나눠보세요!</h2>
+        <div>
+          <p>북링크를 통해 더욱 쉽게 모임을 시작할 수 있어요.</p>
+          <p>내 주변에서 독서 모임을 모집하거나 찾아볼 수 있습니다.</p>
+        </div>
+      </Styled.CommunityBannerTitleDiv>
+      <Styled.CommunityBannerButtonDiv>
+        <button>
+          <span>독서 모임 모집하기</span>
+        </button>
+      </Styled.CommunityBannerButtonDiv>
+    </Styled.CommunityBannerDiv>
+  );
+};
+
+export default CommunityBanner;
