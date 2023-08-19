4fe9b93098dc9ffaa5ebfa1bc1dc1e110b61a9e8
jyj1111 <jyj202040@gmail.com>
2023-08-20 00:48:59 +0900

    chore: 소켓프록시 설정

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default defineConfig({
+  define: {
+    global: 'globalThis',
+  },
   plugins: [react()],
   server: {
     proxy: {
+      '/ws/chat': {
+        target: 'http://13.124.159.21:8080/',
+        ws: true,
+      },
       '/api/v1': {
         target: 'http://13.124.159.21:8080/',
+        ws: true,
       },
     },
   },
