aea26894df775b77e2a6daf7659021fe26f101f8
donghyun-git <donghyun1113@hanmail.net>
2023-11-05 21:40:13 +0900

    fix: 예외처리

--- src/services/mypage/useGetMyBookLinkQuery.js
+++ src/services/mypage/useGetMyBookLinkQuery.js
@@ -1,11 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
+import { useUserStore } from '../../store/useUserStore';
 
 import { getMyBookLinkGetFetch } from '../../lib/apis/mypage/getMyBookLinkGetFetch';
 
 export const useGetMyBookLinkQuery = () =>
   useQuery(['@mypage'], {
     queryFn: async () => {
-      const res = await getMyBookLinkGetFetch();
+      const { isLogin } = useUserStore.getState();
+
+      const res = (isLogin && (await getMyBookLinkGetFetch())) || {
+        data: { data: { profile: false } },
+      };
 
       return res.data.data;
     },
